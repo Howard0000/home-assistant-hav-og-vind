6916fab25c84785819e5d1c8beca4b82e560c7f4
Håvard <146569636+Howard0000@users.noreply.github.com>
2026-02-11 21:23:03 +0100

    Update const.py

--- custom_components/hav_og_vind/const.py
+++ custom_components/hav_og_vind/const.py
@@ -5,6 +5,9 @@ DOMAIN = "hav_og_vind"
 # Oppdateringsintervall (minutter) brukt av koordinatoren
 DEFAULT_SCAN_MINUTES = 30
 
+CONF_SCAN_INTERVAL_MINUTES = "scan_interval_minutes"
+
+
 USER_AGENT = "HavOgVind/0.1 (Home Assistant; contact: 146569636+Howard0000@users.noreply.github.com)"
 
 
@@ -13,4 +16,5 @@ WIND_URL = "https://api.met.no/weatherapi/locationforecast/2.0/complete"
 
 
 HAVVARSEL_TEMP_URL_FMT = "https://api.havvarsel.no/apis/duapi/havvarsel/v2/temperatureprojection/{lon}/{lat}"
+
 HAVVARSEL_SALINITY_URL_FMT = "https://api.havvarsel.no/apis/duapi/havvarsel/v2/dataprojection/salinity/{lon}/{lat}"
